b6b801a96baee6392ebc56190d3c020351099c58
CAO Zheng <caozheng@nuist.edu.cn>
2019-10-21 16:59:25 +0800

    Add solution#2

--- 001.py
+++ 001.py
@@ -7,3 +7,12 @@ class Solution:
             if target - value in dictMap:
                 return dictMap[target - value] + 1, index + 1
             dictMap[value] = index
+
+# Old Solution
+
+class Solution:
+	def twoSum(self, num, target):
+		for i in range(len(num)):
+			for j in range(len(num)-1):
+				if num[j+1] == target - num[i]:
+					return [i, j+1]
